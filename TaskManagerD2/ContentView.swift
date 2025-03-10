138605e32b99dd6b257348d9e21d8639b7ed301f
Rawann0m <raawannm@gmail.com>
2025-03-10 12:47:59 +0300

    Initial Commit

---
+++ TaskManagerD2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskManagerD2
+//
+//  Created by Rawan on 10/09/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskManagerD2/TaskManagerD2App.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerD2App.swift
+//  TaskManagerD2
+//
+//  Created by Rawan on 10/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerD2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
